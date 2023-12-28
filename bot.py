279786c8657cab19513c931fe6c16ff39081e903
NineTailFoxy <oh5457@gmail.com>
2023-12-28 21:18:37 +0900

    Replace twitter.com or x.com with vxtwitter.com
    Replace twitter.com or x.com with vxtwitter.com

--- bot.py
+++ bot.py
@@ -18,6 +18,30 @@ async def on_ready():
     print(f"Bot is online")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for pixiv links"))
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if ("twitter.com/" in message.content or "x.com/" in message.content) and not message.content.startswith("twitter.com") and not message.content.startswith("x.com"):
+        urls = message.content.split()
+        modified_urls = []
+
+        for url in urls:
+            if "twitter.com" in url or "x.com" in url:
+                # Replace twitter.com or x.com with vxtwitter.com
+                modified_url = url.replace("twitter.com", "vxtwitter.com").replace("x.com", "vxtwitter.com")
+                modified_urls.append(modified_url)
+            else:
+                modified_urls.append(url)
+
+        modified_content = ' '.join(modified_urls)
+
+        await message.reply(modified_content, allowed_mentions=discord.AllowedMentions.none())
+
+    await bot.process_commands(message)
+
+
 try:
     bot.run(os.environ.get("TOKEN"))
 except Exception as err:
